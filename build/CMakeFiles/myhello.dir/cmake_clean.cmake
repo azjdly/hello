bb3b4fe6f833b0e280ce9822c23e051bc344564e
li <912479498@qq.com>
2024-10-24 01:26:32 +0800

    no.2

---
+++ build/CMakeFiles/myhello.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myhello.dir/src/hello.c.obj"
+  "CMakeFiles/myhello.dir/src/hello.c.obj.d"
+  "CMakeFiles/myhello.dir/src/main.c.obj"
+  "CMakeFiles/myhello.dir/src/main.c.obj.d"
+  "libmyhello.dll.a"
+  "myhello.exe"
+  "myhello.exe.manifest"
+  "myhello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myhello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
