eb928e9e264a02836602f793f2e358d09719e5c6
li <912479498@qq.com>
2024-10-24 01:41:04 +0800

    no.3

--- build/CMakeFiles/myhello.dir/cmake_clean.cmake
+++ build/CMakeFiles/myhello.dir/cmake_clean.cmake
@@ -1,12 +1,12 @@
 file(REMOVE_RECURSE
+  "C:/Users/91247/Desktop/my_test/hello/bin/libmyhello.dll.a"
+  "C:/Users/91247/Desktop/my_test/hello/bin/myhello.exe"
+  "C:/Users/91247/Desktop/my_test/hello/bin/myhello.exe.manifest"
+  "C:/Users/91247/Desktop/my_test/hello/bin/myhello.pdb"
   "CMakeFiles/myhello.dir/src/hello.c.obj"
   "CMakeFiles/myhello.dir/src/hello.c.obj.d"
   "CMakeFiles/myhello.dir/src/main.c.obj"
   "CMakeFiles/myhello.dir/src/main.c.obj.d"
-  "libmyhello.dll.a"
-  "myhello.exe"
-  "myhello.exe.manifest"
-  "myhello.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
--- build/CMakeFiles/myhello.dir/flags.make
+++ build/CMakeFiles/myhello.dir/flags.make
@@ -4,7 +4,7 @@
 # compile C with C:/mingw64/bin/gcc.exe
 C_DEFINES = 
 
-C_INCLUDES = 
+C_INCLUDES = @CMakeFiles/myhello.dir/includes_C.rsp
 
 C_FLAGS = -g -std=gnu99
 
